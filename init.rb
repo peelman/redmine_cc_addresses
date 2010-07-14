22acf44d82e65d3e49f5e5aac694cc9a8b8c781a
Nick Peelman <nick@peelman.us>
2010-07-13 20:42:44 -0400

    CC works!

--- init.rb
+++ init.rb
@@ -10,6 +10,10 @@ Dispatcher.to_prepare :redmine_cc_addresses do
   unless Issue.included_modules.include? RedmineCcAddresses::IssuePatch
     Issue.send(:include, RedmineCcAddresses::IssuePatch)
   end
+
+  unless Mailer.included_modules.include? RedmineCcAddresses::MailerPatch
+    Mailer.send(:include, RedmineCcAddresses::MailerPatch)
+  end
 end
 
 require_dependency 'cc_addresses_issue_show_hook'
---
+++ lib/redmine_cc_addresses/mailer_patch.rb
@@ -0,0 +1,21 @@
+module RedmineCcAddresses
+  # Patches Redmine's Mailer Model dynamically
+  module MailerPatch
+    def self.included(base) # :nodoc:
+      # Same as typing in the class
+      base.send(:include, MailerInstanceMethods)
+      base.class_eval do
+        alias_method_chain :issue_edit, :cc_addresses
+      end
+    end
+  end
+
+  module MailerInstanceMethods
+    def issue_edit_with_cc_addresses(journal)
+      issue = journal.journalized.reload
+      issue_edit_without_cc_addresses(journal)
+      cc_addresses = issue.cc_addresses.collect {|m| m.mail}
+      cc << cc_addresses
+    end
+  end
+end
