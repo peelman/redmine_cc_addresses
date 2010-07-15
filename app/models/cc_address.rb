819be395b7adaa783b9584d78b2a6bf1fa55f17e
Nick Peelman <nick@peelman.us>
2010-07-15 11:21:12 -0400

    Removing :mail uniqueness validation...

--- app/models/cc_address.rb
+++ app/models/cc_address.rb
@@ -1,6 +1,5 @@
 class CcAddress < ActiveRecord::Base
   belongs_to :issue
   validates_presence_of :issue_id, :mail
-  validates_uniqueness_of :mail, :scope => :issue_id
   validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
