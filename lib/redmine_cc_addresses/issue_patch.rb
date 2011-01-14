54b991520d41a1fb3a9193dd6b60e663430f0258
Nick Peelman <nick@peelman.us>
2011-01-14 07:08:22 -0800

    Taking edavis10’s advice and making the issue model extension unloadable...

--- lib/redmine_cc_addresses/issue_patch.rb
+++ lib/redmine_cc_addresses/issue_patch.rb
@@ -5,6 +5,7 @@ module RedmineCcAddresses
     def self.included(base) # :nodoc:
       # Same as typing in the class
       base.class_eval do
+	unloadable
         belongs_to :deliverable
         has_many :cc_addresses, :class_name => 'CcAddress', :foreign_key => 'issue_id', :dependent => :delete_all
         accepts_nested_attributes_for :cc_addresses, :reject_if => lambda { |cc_address| cc_address[:mail].blank? }, :allow_destroy => true
