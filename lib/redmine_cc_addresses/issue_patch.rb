5b8d8c3c855ced229755b27000e4f3a54b35dfea
Nick Peelman <nick@peelman.us>
2011-07-25 15:29:24 -0400

    Adding safe_attributes line for issue patch.  Fixes bug #1.

--- lib/redmine_cc_addresses/issue_patch.rb
+++ lib/redmine_cc_addresses/issue_patch.rb
@@ -5,7 +5,8 @@ module RedmineCcAddresses
     def self.included(base) # :nodoc:
       # Same as typing in the class
       base.class_eval do
-	unloadable
+        safe_attributes 'cc_addresses_attributes'
+        unloadable
         belongs_to :deliverable
         has_many :cc_addresses, :class_name => 'CcAddress', :foreign_key => 'issue_id', :dependent => :delete_all
         accepts_nested_attributes_for :cc_addresses, :reject_if => lambda { |cc_address| cc_address[:mail].blank? }, :allow_destroy => true
